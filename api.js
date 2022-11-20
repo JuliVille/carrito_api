35900761759c3557186966304ae80dfc5eb3fa50
Julian <julianvillero16@gmail.com>
2022-11-20 15:21:15 -0500

    agregar carrito

--- api.js
+++ api.js
@@ -1,6 +1,15 @@
 const productos = document.getElementById("productos");
+const items = document.getElementById("items");
 const fragment = document.createDocumentFragment();
+const templateProductos = document.getElementById("template-productos").content
+const templateItems = document.getElementById("template-items").content
+
+let carrito = {};
+
 document.addEventListener('DOMContentLoaded', e => {fetchData()})
+document.addEventListener('click', e=>{agregarCarrito(e)})
+
+
 const fetchData = async () => {
     const res = await fetch('api.json');
     const data = await res.json();
@@ -8,7 +17,6 @@ const fetchData = async () => {
 }
 
 const pintarCards = data =>{
-    const templateProductos = document.getElementById("template-productos").content
     data.forEach(item =>{
         templateProductos.querySelector('h5').textContent=item.titulo;
         templateProductos.querySelector('p').textContent=item.precio;
@@ -18,4 +26,37 @@ const pintarCards = data =>{
         fragment.appendChild(clone);
     })
     productos.appendChild(fragment);
+}
+
+const agregarCarrito = e =>{
+    llenarCarro(e.target.parentElement)
+}
+
+const llenarCarro = item =>{
+    const producto = {
+        id: item.querySelector('button').dataset.id,
+        precio: item.querySelector('p').textContent,
+        titulo: item.querySelector('h5').textContent,
+        cantidad: 1
+    }
+
+    carrito[producto.id] = {...producto}
+    pintarProductos();
+}
+
+const pintarProductos = () =>{
+    items.innerHTML='';
+    Object.values(carrito).forEach(producto =>{
+        templateItems.querySelector('th').textContent=producto.id;
+        templateItems.querySelectorAll('td')[0].textContent = producto.titulo;
+        templateItems.querySelectorAll('td')[1].textContent = producto.cantidad;
+        templateItems.querySelector('span').textContent=producto.precio * producto.cantidad;
+
+        templateItems.querySelector('.btn-info').dataset.id=producto.id;
+        templateItems.querySelector('.btn-danger').dataset.id=producto.id;
+
+        const clone = templateItems.cloneNode(true);
+        fragment.append(clone);
+    })
+    items.appendChild(fragment)
 }
